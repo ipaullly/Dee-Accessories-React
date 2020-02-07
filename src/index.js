20cb9d57e5802b4e4faeaefa7c6bb22c57ff9dd1
ipaullly <ipaully22@gmail.com>
2020-02-07 20:19:51 +0300

    initialize redux and setup root reducer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
