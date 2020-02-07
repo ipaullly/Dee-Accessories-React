3372bfa99899a8935342a7d8cb12de53d1964c13
ipaullly <ipaully22@gmail.com>
2020-02-07 20:32:59 +0300

    setup store and initialize first action creator

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
